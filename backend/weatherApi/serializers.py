58d739e8e3d8979a282999d8f69cd3ef70743d95
Abhinav Yadav <abhinav.sny.2002@gmail.com>
2023-10-22 16:08:50 +0530

    removed old indexes

--- backend/weatherApi/serializers.py
+++
@@ -1,8 +0,0 @@
-# weatherapi/serializers.py
-from rest_framework import serializers
-from weatherData.models import WeatherData
-
-class WeatherDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeatherData
-        fields = '__all__'
--- backend/weatherApi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from weatherApi.views import FetchAndStoreWeatherData
-
-urlpatterns = [
-    path('weather/<str:location>/', FetchAndStoreWeatherData.as_view(), name='weather-data'),
-]
