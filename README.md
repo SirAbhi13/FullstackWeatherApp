bcd6f1f3caea2871c88e3c5b616a6484dda2578a
Abhinav Yadav <abhinav.sny.2002@gmail.com>
2023-10-22 20:40:10 +0530

    implement caching

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Weather App.
-
+ Built Using 
+ - ReactJS
+ - Django
+ - DjangoRestFramework
+ - Openweather API
 
 ## Setup Guidelines
+
+- Clone the repository.
+- create .env file and copy the keys from `.env.example`. You will need the API key from https://openweather.org. Fill the relevant DB values with the keys.
+- create a virtual python environment and install the neccessary packages using the given `src/requirements.txt`
+- cd into src.
+- run the command `python3 manage.py migrate`
+- After successful migration, start the server using `python3 manage.py runserver`
+- The website should be available at http://127.0.0.1:8000/
+- Start entering correct city names and see the Weather!
+
+## Challenges I faced while creating this project
+
+
+## Snapshot of the App
+![image](./src/frontend/src/assets/Screenshot%202023-10-22%20194310.png)
--- src/weatherApi/views.py
+++ src/weatherApi/views.py
@@ -7,7 +7,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from weatherData.models import WeatherData
 from .serializers import WeatherDataSerializer
-import json
 from datetime import datetime
 import pytz
 import logging
@@ -23,43 +22,51 @@ base_url = "https://api.openweathermap.org/data/2.5"
 class FetchAndStoreWeatherData(APIView):
     def get(self, request, location):
         try:
-            # Make a request to the OpenWeather API
-            url = f"{base_url}/weather?q={location}&units=metric&appid={api_key}"
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json()
-
-            # Extract relevant data from the API response
-            temperature = data['main']['temp']
-            feels_like_temp = data['main']['feels_like']
-            humidity = data['main']['humidity']
-            wind_speed = data['wind']['speed']
-            cloud_cover = data['clouds']['all']
-            visibility = data.get('visibility', 0) 
-            weather_desc = data['weather'][0]['description']
-            icon = data['weather'][0]['icon']
-            forecast_data = get_forecast_data(location)
-
-            # Create a new WeatherData object and save it to the database
-            weather_data = WeatherData(
-                location=location,
-                temperature=temperature,
-                feels_like_temp=feels_like_temp,
-                humidity=humidity,
-                wind_speed=wind_speed,
-                cloud_cover=cloud_cover,
-                visibility=visibility,
-                weather_desc=weather_desc,
-                icon=icon,
-                forecast_data=forecast_data
-            )
-            weather_data.save()
-            logger.info('Stored weather Data in DB', weather_data)
-            # Serialize and return the stored data
+            weather_data = WeatherData.objects.get(location=location)
+            print('acha hai database wala code?')
             serializer = WeatherDataSerializer(weather_data)
             return Response(serializer.data)
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+        except WeatherData.DoesNotExist:
+        
+            try:
+                # Make a request to the OpenWeather API
+                url = f"{base_url}/weather?q={location}&units=metric&appid={api_key}"
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+
+                # Extract relevant data from the API response
+                temperature = data['main']['temp']
+                feels_like_temp = data['main']['feels_like']
+                humidity = data['main']['humidity']
+                wind_speed = data['wind']['speed']
+                cloud_cover = data['clouds']['all']
+                visibility = data.get('visibility', 0) 
+                weather_desc = data['weather'][0]['description']
+                icon = data['weather'][0]['icon']
+                forecast_data = get_forecast_data(location)
+
+                # Create a new WeatherData object and save it to the database
+                weather_data = WeatherData(
+                    location=location,
+                    temperature=temperature,
+                    feels_like_temp=feels_like_temp,
+                    humidity=humidity,
+                    wind_speed=wind_speed,
+                    cloud_cover=cloud_cover,
+                    visibility=visibility,
+                    weather_desc=weather_desc,
+                    icon=icon,
+                    forecast_data=forecast_data
+                )
+                weather_data.save()
+                logger.info('Stored weather Data in DB', weather_data)
+                # Serialize and return the stored data
+                serializer = WeatherDataSerializer(weather_data)
+                print('api wala code?')
+                return Response(serializer.data)
+            except Exception as e:
+                return Response({"error": str(e)}, status=500)
         
 
 
